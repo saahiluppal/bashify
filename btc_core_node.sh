c23013c4a3a2c0739bbeb85963820db4c33fed9d
Sahil Uppal <sahil2kuppal@gmail.com>
2019-10-12 14:49:55 +0530

    Running core node

---
+++ btc_core_node.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/bash
+
+echo "Warning: This is for RPM based Distributions only;"
+echo "Installing Dependencies..."
+
+sudo dnf install autoconf automake libtool gcc-c++ libdb4-cxx-devel boost-devel openssl-devel libevent-devel
+
+echo "Going Home"
+
+cd $HOME
+git clone https://github.com/bitcoin/bitcoin.git
+
+cd bitcoin
+git checkout v0.18.1
+git status
+
+echo "Running Autogen"
+./autogen.sh
+
+sleep 2
+
+echo "Configuring"
+./configure --disable-dependency-tracking
+
+# Install make if you don't have
+
+sudo dnf install make  
+make # -j4 (If required)
+sudo make install
+
+echo "Successfully installed BTC_Core_NOde"
+
+
