5bb08724a2e65749294a4d0dd2ba3a13caef2bf2
Sahil Uppal <sahil2kuppal@gmail.com>
2019-11-16 21:24:10 +0530

    done for fedora

--- vbox.sh
+++ vbox.sh
@@ -1,13 +1,29 @@
 #!/usr/bin/bash
 
-echo "Installing dependencies"
+read -p "Debian/Fedora [D/f]:" VAR
+VAR=${VAR:-'D'}
+shopt -s nocasematch
 
-sudo dnf install @development-tools
-sudo dnf install kernel-devel kernel-headers dkms qt5-qtx11extras  elfutils-libelf-devel zlib-devel
+case "$VAR" in 
+    "d" )
+	echo "Yet to implement"
+	;;
+    "f" )
+
+	echo "Fedora::"
+	echo "Installing dependencies"
+
+	sudo dnf install @development-tools
+	sudo dnf install kernel-devel kernel-headers dkms qt5-qtx11extras  elfutils-libelf-devel zlib-devel
 
 
-wget https://download.virtualbox.org/virtualbox/6.0.14/VirtualBox-6.0-6.0.14_133895_fedora29-1.x86_64.rpm
+	wget https://download.virtualbox.org/virtualbox/6.0.14/VirtualBox-6.0-6.0.14_133895_fedora29-1.x86_64.rpm
 
-sudo rpm -i VirtualBox-6.0-6.0.12_133076_fedora29-1.x86_64.rpm
+	sudo rpm -i VirtualBox-6.0-6.0.12_133076_fedora29-1.x86_64.rpm
 
-sudo usermod -a -G vboxusers $LOGNAME
+	sudo usermod -a -G vboxusers $LOGNAME
+	;;
+    * ) 
+	echo "Skipping"
+	;;
+esac
