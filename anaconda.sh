1a8785e2fa1facbf89970ee38a4f5545d003fd11
Sahil Uppal <sahiluppal2k@gmail.com>
2019-11-24 19:54:59 +0530

    case link updated

--- anaconda.sh
+++ anaconda.sh
@@ -15,7 +15,8 @@ case "$VAR" in
         cd
 	    conda activate
         conda config --set auto_activate_base false
-	    mv $HOME/linux_sup/Anaconda3-2019.07-Linux-x86_64.sh $HOME/Downloads/;;
+	    mv $HOME/linux_sup/Anaconda3-2019.07-Linux-x86_64.sh $HOME/Downloads/
+        ;;
     "m" )
         echo "Installation for Anaconda"
         echo "1. MacOS"
@@ -30,14 +31,15 @@ case "$VAR" in
                 wget https://repo.anaconda.com/archive/Anaconda3-2019.07-MacOSX-x86_64.pkg
                 bash Anaconda3-2019.07-Linux-x86_64.sh;;
             2 )
-		echo "Default Linux 64bit python 3.7"
-		echo "Downloading Anaconda Distribution"
-		wget https://repo.anaconda.com/archive/Anaconda3-2019.07-Linux-x86_64.sh
-		bash Anaconda3-2019.07-Linux-x86_64.sh
-		source $HOME/.bashrc
-		conda activate
-		conda config --set auto_activate_base false
-		mv Anaconda3-2019.07-Linux-x86_64.sh $HOME/Downloads/;;
+                echo "Default Linux 64bit python 3.7"
+                echo "Downloading Anaconda Distribution"
+                wget https://repo.anaconda.com/archive/Anaconda3-2019.07-Linux-x86_64.sh
+                bash Anaconda3-2019.07-Linux-x86_64.sh
+                source $HOME/.bashrc
+                conda activate
+                conda config --set auto_activate_base false
+                mv Anaconda3-2019.07-Linux-x86_64.sh $HOME/Downloads/
+                ;;
         esac;;
     *)
         echo "Skipping"
